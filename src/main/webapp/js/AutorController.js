0d624804096c5b8188e51a7d798f600dd661afa9
FredyNicolasMoreno <fredy.moreno01@uptc.edu.co>
2019-10-16 19:12:02 -0500

    Tablas Creadas

--- src/main/webapp/js/AutorController.js
+++ src/main/webapp/js/AutorController.js
@@ -1,95 +1,92 @@
 'use strict';
 
 var listaAutor = [];
-var consecutivoAutor = 2;
+var consecutivoAutor = 0;
+var listAutorJson = [];
 
 module.controller('AutorCtrl', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
         //listar
-        $http.get('./webresources/ServicioAutor', {})
+        $scope.listaA = listAutorJson;
+        $http.get('./webresources/AuthorService', {})
                 .success(function (data, status, headers, config) {
                     //$scope.lista = data;
                     listaAutor = data;
-                    $scope.lista = listaAutor;
-                    $scope.datosFormulario = {};
+                    var j;
+                    listAutorJson = [];
+                    for (var i = 0; i < listaAutor.length; i++) {
+                        listAutorJson.push({
+                            "id": listaAutor[i].id,
+                            "name": listaAutor[i].name,
+                            "nationality": listaAutor[i].nationality
+                        });
+                    }
+                    $scope.listaA = listAutorJson;
+                    $scope.listAuthor = listJson;
+                    console.log("face23")
+                    $scope.datosFormularioAutor = {};
                     $scope.panelEditar = false;
                     $scope.listar = function () {
-                        $scope.lista = listaAutor;
+                        $scope.listaA = listAutorJson;
                     };
                     $scope.listar();
                 }).error(function (data, status, headers, config) {
             alert('Error al consultar la informaci\xf3n, por favor intente m\xe1s tarde');
         });
-
+        $scope.listar = function () {
+            $scope.listaA = listAutorJson;
+            console.log(listaA);
+            consecutivoAutor = listAutorJson.length;
+        };
         //guardar
-        $scope.nuevo = function () {
+        $scope.nuevoAutor = function () {
             $scope.panelEditar = true;
-            $scope.datosFormulario = {};
+            $scope.datosFormularioAutor = {};
 
         };
 
-        $scope.guardar = function () {
+        $scope.guardarAutor = function () {
             $scope.errores = {};
             var error = false;
 
             if (error)
                 return;
-            if (!$scope.datosFormulario.id) {
-                $scope.datosFormulario.id = consecutivoAutor++;
-
+            if (!$scope.datosFormularioAutor.id) {
+                $scope.datosFormularioAutor.id = consecutivoAutor++;
+                console.log(consecutivoAutor);
             }
-
-            $scope.lista.push($scope.datosFormulario);
-            $http.post('./webresources/ServiciosAutor/', $scope.datosFormulario.nombre).then(
+            alert("Datos guardados con exito");
+            var autor = [$scope.datosFormularioAutor.id,
+                $scope.datosFormularioAutor.nombre,
+                $scope.datosFormularioAutor.nacionalidad];
+            $http.post('./webresources/AuthorService/').then(
                     function successCallback(response) {
+                        console.log("Successfully POST-ed data");
                     },
                     function errorCallback(response) {
                         console.log("POST-ing of data failed");
                     }
             );
-            alert("Sus datos han sido guardados con éxito");
             $scope.cancelar();
         };
         $scope.cancelar = function () {
             $scope.panelEditar = false;
-            $scope.datosFormulario = {};
+            $scope.datosFormularioAutor = {};
         };
 
         //editar 
         $scope.editar = function (editado) {
             $scope.panelEditar = true;
-            $scope.datosFormulario = editado;
-            console.log(editado);
-            var data = $.param({
-                id: $scope.firstName,
-                nombre: $scope.lastName
-            });
-            $http.put('./webresources/ServiciosAutor/', JSON.stringify(editado)).then(function (response) {
-                if (response.data)
-                    $scope.msg = "Put Data Method Executed Successfully!";
-            }, function (response) {
-                $scope.msg = "Service not Exists";
-                $scope.statusval = response.status;
-                $scope.statustext = response.statusText;
-                $scope.headers = response.headers();
-            });
+            $scope.datosFormularioAutor = editado;
         };
         //eliminar
         $scope.eliminar = function (data) {
-            for (var i = 0; i < $scope.lista.length; i++) {
-                if ($scope.lista[i].id == data.id) {
-                    $scope.lista.splice(i, 1);
+            if (confirm('\xbfDesea elminar este registro?')) {
+                for (var i = 0; i < $scope.lista.length; i++) {
+                    if ($scope.lista[i].id == data.id) {
+                        $scope.lista.splice(i, 1);
+                    }
                 }
             }
-//            $http({
-//                method: 'DELETE',
-//                url: './webresources/ServiciosAutor/' + data.id
-//            }).then(function successCallback(response) {
-//                alert("User has deleted Successfully");
-//                var index = $scope.data.indexOf(user);
-//                $scope.users.splice(index, 1);
-//            }, function errorCallback(response) {
-//                alert("Error. while deleting user Try Again!");
-//            });
         };
     }]);
 
